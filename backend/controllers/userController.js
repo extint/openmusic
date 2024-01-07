87d7854c0648b915a738d26d215734956f0adff1
Warren Jacinto <warrenjacinto@pop-os.localdomain>
2024-01-07 20:59:57 +0530

    addded friends backend calls

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -59,3 +59,69 @@ module.exports.unlikeSong = async (req, res) => {
         }
     }
 }
+
+module.exports.getUserDetails = async (req, res) => {
+    const userName = req.body.userName;
+
+    const check = await userCollection.findOne({userName : userName});
+
+    if(!check){
+        return res.status(404).json({message: "User does not exist"});
+    }
+    else{
+        const user = {
+            userName: check.userName,
+            emailId: check.emailId,
+            profilePhotoFilePath: check.profilePhotoFilePath,
+            //to complete
+        }
+
+        return res.status(200).json({user: user});
+    }
+}
+
+module.exports.addFriend = async (req,res) => {
+    const user = req.body.userName;
+    const friend = req.body.friendName;
+
+    const check1 = await userCollection.findOne({userName: user});
+    const check2 = await userCollection.findOne({userName: friend});
+
+    if(!check1 || !check2){
+        return res.status(404).json({message: "User does not exist"});
+    }
+    else{
+        userCollection.updateOne({userName: user}, {$push: {friends: friend}})
+        .then(()=> { return res.status(200).json({message: "Successfully added friend"})})
+        .catch((err)=>{ 
+            console.log(err); 
+            return res.status(500).json({message: "Internal Server Error"})
+        });
+    }
+}
+
+module.exports.removeFriend = async (req, res) => {
+    const user = req.body.userName;
+    const friend = req.body.friendName;
+
+    const check1 = await userCollection.findOne({userName: user});
+    const check2 = await userCollection.findOne({userName: friend});
+
+    if(!check1 || !check2){
+        return res.status(404).json({message: "User does not exist"});
+    }
+    else{
+        if(check1.friends.includes(friend)){
+            userCollection.updateOne({userName: user}, {$pullAll: {friends: [friend]}})
+            .then(()=> { return res.status(200).json({message: "Successfully removed friend"})})
+            .catch((err)=>{ 
+                console.log(err); 
+                return res.status(500).json({message: "Internal Server Error"})
+            });
+
+        }
+        else{
+            return res.status(404).json({message: "Given users are not friends"});
+        }
+    }
+}
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -4,6 +4,14 @@ const router = express.Router();
 
 const userController = require('../controllers/userController');
 
+router.route('/friend').post(userController.addFriend);
+
+// router.route('/friend').get(userController.getFriendsList); // To do
+
+router.route('/friend').delete(userController.removeFriend); 
+
+// router.route('/').get(userController.getUserDetails); // To do
+
 router.route('/like').post(userController.addLikedSong);
 
 router.route('/like').get(userController.getLikedSongs);
