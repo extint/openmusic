3d16cc10dc613f7299c0c0dd5a99eefaa8da51da
Vedant Mehra <vedantmehra20@gmail.com>
2024-01-18 17:04:11 +0530

    Pushing HomePage src

---
+++ frontend/src/HomePage/MainHome/mainplaylists.js
@@ -0,0 +1,107 @@
+import React from "react";
+import "./mainplaylists.css";
+export const MainHome = () => {
+    return (
+        // <div className="box">
+            <div className="main-home">
+                {/* <div className="overlap-group"> */}
+                    {/* <div className="div" /> */}
+                    <div className="text-wrapper">WELCOME yathu !</div>
+                    <div className="text-wrapper-2">recently played:</div>
+                    <div className="playlist-boards">
+                        <div className="playlist-board">
+                            <img className="model" alt="Model" src="model.png" />
+                            <div className="song" />
+                            <div className="song-2" />
+                        </div>
+                        <div className="playlist-board">
+                            <img className="model" alt="Model" src="model.png" />
+                            <div className="song" />
+                            <div className="song-2" />
+                        </div>
+                        <div className="playlist-board">
+                            <img className="model" alt="Model" src="model.png" />
+                            <div className="song" />
+                            <div className="song-2" />
+                        </div>
+                        <div className="playlist-board">
+                            <img className="model" alt="Model" src="model.png" />
+                            <div className="song" />
+                            <div className="song-2" />
+                        </div>
+                        <div className="playlist-board">
+                            <img className="model" alt="Model" src="model.png" />
+                            <div className="song" />
+                            <div className="song-2" />
+                        </div>
+                        <div className="playlist-board">
+                            <img className="model" alt="Model" src="model.png" />
+                            <div className="song" />
+                            <div className="song-2" />
+                        </div>
+                        <div className="playlist-board">
+                            <img className="model" alt="Model" src="model.png" />
+                            <div className="song" />
+                            <div className="song-2" />
+                        </div>
+                        <div className="playlist-board">
+                            <img className="model" alt="Model" src="model.png" />
+                            <div className="song" />
+                            <div className="song-2" />
+                        </div>
+                        <div className="playlist-board">
+                            <img className="model" alt="Model" src="model.png" />
+                            <div className="song" />
+                            <div className="song-2" />
+                        </div>
+                        <div className="playlist-board">
+                            <img className="model" alt="Model" src="model.png" />
+                            <div className="song" />
+                            <div className="song-2" />
+                        </div>
+                        <div className="playlist-board">
+                            <img className="model" alt="Model" src="model.png" />
+                            <div className="song" />
+                            <div className="song-2" />
+                        </div>
+                        <div className="playlist-board">
+                            <img className="model" alt="Model" src="model.png" />
+                            <div className="song" />
+                            <div className="song-2" />
+                        </div>
+                        <div className="playlist-board">
+                            <img className="model" alt="Model" src="model.png" />
+                            <div className="song" />
+                            <div className="song-2" />
+                        </div>
+                        <div className="playlist-board">
+                            <img className="model" alt="Model" src="model.png" />
+                            <div className="song" />
+                            <div className="song-2" />
+                        </div>
+                        <div className="playlist-board">
+                            <img className="model" alt="Model" src="model.png" />
+                            <div className="song" />
+                            <div className="song-2" />
+                        </div>
+                        <div className="playlist-board">
+                            <img className="model" alt="Model" src="model.png" />
+                            <div className="song" />
+                            <div className="song-2" />
+                        </div>
+                        <div className="playlist-board">
+                            <img className="model" alt="Model" src="model.png" />
+                            <div className="song" />
+                            <div className="song-2" />
+                        </div>
+                        <div className="playlist-board">
+                            <img className="model" alt="Model" src="model.png" />
+                            <div className="song" />
+                            <div className="song-2" />
+                        </div>
+                        
+                    </div>
+            </div>
+        // </div>
+    );
+};
---
+++ frontend/src/HomePage/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Navbar.css";
+
+export const Navbar = () => {
+    return (
+        <div className="navbox" >
+            {/* <img className="Navbar" alt="Navbar" src="navbar.png" /> */}
+            {/* <div className="topbar" /> */}
+            <div className="text-wrapper" >MYUSIK</div>
+        </div>
+    );  
+};
+
