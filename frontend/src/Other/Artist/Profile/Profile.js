3f682cba03a416df23a6bd8531061d12088d4a1d
lbhnsh <labhanshvnaik2104@gmail.com>
2024-02-07 21:18:38 +0530

    added profile part

---
+++ frontend/src/Other/Artist/Profile/Profile.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Profile.css";
+import { useEffect,useState } from "react";
+
+export const Profile = () => {
+
+  return (
+    <div className="Profile">
+        
+          <div className="photo-bg">
+            <img className="photo" alt="Photo" src="model1.png" />
+          </div>
+          <div className="text-content">
+            <div className="user">user</div>
+            <div className="username">Labhansh</div>
+          <div className="profile-playlists">4 public playlists</div>
+          <div className="followers">42 followers</div>
+          <div className="following">0 following</div>
+          </div>
+        
+      </div>
+  );
+};
