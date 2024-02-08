f3eb05e5606206789b42e89d9f7156e874a3a488
Warren Jacinto <warrenjacinto@gmail.com>
2024-02-08 18:49:00 +0530

    sanity check

--- backend/index.js
+++ backend/index.js
@@ -6,13 +6,15 @@ const mongoose = require("mongoose")
 const password = encodeURIComponent("semicolonizer");
 const dbURI = `mongodb+srv://opium:${password}@nodet.qugbjpj.mongodb.net/nodet?retryWrites=true&w=majority`;
 const cors = require('cors');
-const {getArtists, getRecommendedSongs, refreshToken, search} = require('./utils/spotifyapi');
-require("dotenv").config({ override: true});
+const fs = require('fs');
+const path = './.env';
+const { getArtists, getRecommendedSongs, refreshToken, search } = require('./utils/spotifyapi');
+require("dotenv").config({ override: true });
 const { getTracks } = require("./utils/spotifyapi");
 
-app.use(cors({origin: 'http://localhost:3000', credentials:true}));
+app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
 
-mongoose.connect(dbURI,{useNewUrlParser:true})
+mongoose.connect(dbURI, { useNewUrlParser: true })
     .then(() => {
         console.log('Mongoose connected successfully!');
     })
@@ -33,7 +35,12 @@ app.listen(8000, (err) => {
     if (err) {
         console.log(err);
     }
-    setInterval(()=>{
+    if (fs.existsSync(path)) {
+        console.log('env exists');
+    } else {
+        console.error('env file not found, please set up your .env file correctly');
+    }
+    setInterval(() => {
         refreshToken();
     }, 3500000);
     refreshToken();
