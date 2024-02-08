cfd49c998bf15c2b68238f873aea01602eca4f02
Warren Jacinto <warrenjacinto@gmail.com>
2024-02-08 20:07:41 +0530

    search changes

--- backend/controllers/searchController.js
+++ backend/controllers/searchController.js
@@ -2,11 +2,6 @@ const { search } = require("../utils/spotifyapi");
 
 module.exports.search = async (req, res) => {
     console.log("search request");
-    try{
-        return res.status(200).json(await search(req.query.q));
-    }
-    catch(error){
-        console.log(err);
-        return res.status(400).json({message: "An error has occured while searching"});
-    }
+    let out = await search(req.query.q);
+    return res.status(200).json(out);
 }
