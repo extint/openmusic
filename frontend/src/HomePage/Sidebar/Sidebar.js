e91f52d664fb08b79111c63baa1ba0ebb5d1f28e
Vedant Mehra <vedantmehra20@gmail.com>
2024-02-09 00:37:38 +0530

    search done linking left

--- frontend/src/HomePage/Sidebar/Sidebar.js
+++ frontend/src/HomePage/Sidebar/Sidebar.js
@@ -2,32 +2,50 @@ import React, { useState } from "react";
 import "./Sidebar.css";
 import axios from "axios";
 import { Link, useParams } from "react-router-dom";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 export const Sidebar = (props) => {
     const navigate = useNavigate()
     const params = useParams();
-    const [searchQ,setSearchQ]=useState("")
-    const [searchR,setSearchR]=useState("")
-    const searchQuery= async (e)=>{
+    const [searchQ, setSearchQ] = useState("")
+    const [searchR, setSearchR] = useState("")
+    const [searchBlock, setSearchBlock] = useState(false)
+    useEffect(() => {
+        if (searchQ) {
+            setSearchBlock(true)
+            console.log("true kiya useffect mai")
+        } else {
+            setSearchBlock(false)
+        }
+    }, [searchR]);
+
+    const searchQuery = async (e) => {
         // console.log(e.target.value);
         setSearchQ(e.target.value)
-        if(searchQ){
-        try{
-        const searchData = await axios.get("http://localhost:8000/search", {
-            params: {
-              q: searchQ
-            },
-            headers:{
-              'Content-Type': "application/json"
+        if (searchQ) {
+            try {
+                const searchData = await axios.get("http://localhost:8000/search", {
+                    params: {
+                        q: searchQ
+                    },
+                    headers: {
+                        'Content-Type': "application/json"
+                    }
+                });
+                setSearchR(searchData.data);
+                console.log(searchData.data, "This is my search data smiley face :)");
+                if (searchData.data) {
+                    setSearchBlock(true)
+                    console.log(searchBlock, "yahape")
+                }
+                else {
+                    setSearchBlock(false)
+                }
+            } catch (err) {
+                console.log(err);
             }
-          });
-          setSearchR(searchData.data);
-          console.log(searchData.data, "This is my search data smiley face :)");
-        } catch (err) {
-          console.log(err);
         }
-    }
-
+        // setSearchBlock(false)
     };
     return (
         <>
@@ -49,13 +67,28 @@ export const Sidebar = (props) => {
                     </div>
                 </div>
                 <div className="libraryandsearch">
-                    <div className="searchBox">
-                    {/* <div className="tempDiv" style={{display:"flex"}}> */}
+                    <div className="searchBox" onMouseLeave={() => { if (searchBlock) setSearchBlock(false) }}>
+                        {/* <div className="tempDiv" style={{display:"flex"}}> */}
                         <img className="search" alt="Search" src="/search.png" />
-                        <input type="text" className="search-text" placeholder = "Search Anything" onChange={searchQuery}></input>
-                    {/* </div> */}
-                        {/* <div className="preSearchResults"></div> */}
+                        <input type="text" className="search-text" placeholder="Search Anything" onChange={searchQuery}></input>
+                        {/* </div> */}
+                        <div className={searchBlock ? ("SearchResults") : ("preSearchResults")}>
+                            {searchR && searchR.artists.map((item, index) => (
+                                <div className="searchOutput" key={index}>
+                                    <img className="sModel" alt="Model" src={item.images[0].url && item.images[0].url} />
+                                    <div className="sSong">{item.name}</div>
+                                </div>
+                            ))}
+                            {searchR && searchR.tracks.map((item, index) => (
+                                <div className="searchOutput" key={index}>
+                                    <img className="sModel" alt="Model" src={item.images[0].url && item.images[0].url} />
+                                    <div className="sSong">{item.name}</div>
+                                    <br/>
+                                    <div className="sArtist">{item.artists[0].name}</div>
+                                </div>
+                            ))}
                         </div>
+                    </div>
 
                     <img className="playlist" alt="Playlist" src="/playlists.png" />
                 </div>
