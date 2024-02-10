fa8f0a16211d3ec61ff0e919af57f2f0cd3a7b8c
lbhnsh <labhanshvnaik2104@gmail.com>
2024-02-10 11:21:25 +0530

    ;ast

--- frontend/src/Other/User/Profile/Profile.js
+++ frontend/src/Other/User/Profile/Profile.js
@@ -5,17 +5,17 @@ import { useEffect,useState } from "react";
 export const Profile = () => {
 
   return (
-    <div className="Profile">
+    <div className="PProfile">
         
-          <div className="photo-bg">
-            <img className="photo" alt="Photo" src="model1.png" />
+          <div className="Pphoto-bg">
+            <img className="Pphoto" alt="PPhoto" src="https://www.google.com/url?sa=i&url=http%3A%2F%2Fwww.shadowsphotography.co%2Fdiscover-photography-the-art-of-the-image%2F&psig=AOvVaw0tuEJSQFPtU7tzA7MCNtJH&ust=1707630531171000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCLCn2cyJoIQDFQAAAAAdAAAAABAH"/>
           </div>
-          <div className="text-content">
-            <div className="user">user</div>
-            <div className="username">Labhansh</div>
-          <div className="profile-playlists">4 public playlists</div>
-          <div className="followers">42 followers</div>
-          <div className="following">0 following</div>
+          <div className="Ptext-content">
+            <div className="Puser">user</div>
+            <div className="Pusername">WarrenJ</div>
+          <div className="Pprofile-playlists">6 public playlists</div>
+          <div className="Pfollowers">4 followers</div>
+          <div className="Pfollowing">2 following</div>
           </div>
         
       </div>
