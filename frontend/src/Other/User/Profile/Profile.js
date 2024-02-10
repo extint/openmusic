2ea9ae949734dd6d8a3c4d1b6c90e43aee5ba149
lbhnsh <labhanshvnaik2104@gmail.com>
2024-02-10 11:46:55 +0530

    check

--- frontend/src/Other/User/Profile/Profile.js
+++ frontend/src/Other/User/Profile/Profile.js
@@ -8,8 +8,9 @@ export const Profile = () => {
     <div className="PProfile">
         
           <div className="Pphoto-bg">
-            <img className="Pphoto" alt="PPhoto" src="https://www.google.com/url?sa=i&url=http%3A%2F%2Fwww.shadowsphotography.co%2Fdiscover-photography-the-art-of-the-image%2F&psig=AOvVaw0tuEJSQFPtU7tzA7MCNtJH&ust=1707630531171000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCLCn2cyJoIQDFQAAAAAdAAAAABAH"/>
-          </div>
+            <div className="Pphoto">
+            <img className="search" alt="" src="./mode.png"/>
+          </div></div>
           <div className="Ptext-content">
             <div className="Puser">user</div>
             <div className="Pusername">WarrenJ</div>
